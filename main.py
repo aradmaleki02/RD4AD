2cbd75db3d08ba9e2493cce0ccecba4bfd0487c8
Arad Maleki <arad.m.t02@gmail.com>
2024-05-07 19:52:51 +0330

    debug

--- main.py
+++ main.py
@@ -122,19 +122,19 @@ def train(_class_):
 
         if (epoch + 1) % 2 == 0:
             print('Main:')
-            auroc_px, auroc_sp, aupro_px = evaluation(encoder, bn, decoder, test_dataloader1, device)
-            print('Pixel Auroc:{:.3f}, Sample Auroc{:.3f}, Pixel Aupro{:.3}'.format(auroc_px, auroc_sp, aupro_px))
+            auroc_sp = evaluation(encoder, bn, decoder, test_dataloader1, device)
+            print('Sample Auroc{:.3f}'.format(auroc_sp))
             torch.save({'bn': bn.state_dict(),
                         'decoder': decoder.state_dict()}, ckp_path)
             print('=' * 30)
         if (epoch + 1) % 2 == 0:
             print('Shifted:')
-            auroc_px, auroc_sp, aupro_px = evaluation(encoder, bn, decoder, test_dataloader2, device)
-            print('Pixel Auroc:{:.3f}, Sample Auroc{:.3f}, Pixel Aupro{:.3}'.format(auroc_px, auroc_sp, aupro_px))
+            auroc_sp = evaluation(encoder, bn, decoder, test_dataloader2, device)
+            print('Sample Auroc{:.3f}'.format(auroc_sp))
             torch.save({'bn': bn.state_dict(),
                         'decoder': decoder.state_dict()}, ckp_path)
             print('=' * 30)
-    return auroc_px, auroc_sp, aupro_px
+    return auroc_sp
 
 
 
